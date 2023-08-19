504a0484493adfcdab5057f1be276e1e3ecef299
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2023-08-19 11:54:08 +0530

    Add files via upload

---
+++ DSA 19-08/Input.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class Input {
+    public static void main(String[] args) {
+
+//        Taking array from input
+//      there are 3 methods
+
+//        ** Method 1 **
+        
+//        int[] arr = new int[5];
+//
+//        arr[0] = 23;
+//        arr[1] = 34;
+//        arr[2] = 54;
+//        arr[3] = 75;
+//        arr[4] = 64;
+//
+//        System.out.println(arr[3]);
+//        ans will be = 75;
+
+//        ** Method 2 ** (with input by user)
+
+//        Scanner in = new Scanner(System.in);
+//        int[] arr = new int[5];
+
+//        for(int i=0; i < arr.length; i++){
+//            arr[i] = in.nextInt();
+//            }
+
+//        ** Method 3 **
+
+//        System.out.println(Arrays.toString(arr));
+
+//        internally uses for loop and the output in proper format
+//        it will give output = [23, 34, 54, 75, 64];
+
+    }
+}
