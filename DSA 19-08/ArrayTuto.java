c077700b14c91363818848272f51a4bb3d2fb76b
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2023-08-19 11:31:27 +0530

    Add files via upload

---
+++ DSA 19-08/ArrayTuto.java
@@ -0,0 +1,35 @@
+public class ArrayTuto {
+    public static void main(String[] args) {
+
+//        Declaration of ARRAY
+//        Method 1
+//        int[] arr = new int[5];
+
+//        this arr = [0,0,0,0,0]
+//        but for String it will be [null, null, null, null, null]
+
+//        int - datatype
+//        arr - ref variable
+//        new - allocate memory in heap
+//        int[5] - store int that mentioned in bracket for this [0,0,0,0,0]
+
+//        Method 2
+//        int[] arr = {2,3,4,5,6};
+//        direct allocation
+
+//        index of array
+//        [43, 32, 56, 48]
+//         0   1   2   3
+
+//        calling/printing specific number
+//        System.out.println(arr[2]);
+//        it will print index 2 in array
+//        for above ex ans is 56
+
+
+//        int[] arr - compile time
+//        new int[5] - run time
+
+        
+    }
+}
